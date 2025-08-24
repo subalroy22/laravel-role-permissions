c39dc26cd2bdb9da8d1b5d1766630c1d7d735d87
Subol Roy <subalroy5561@gmail.com>
2025-08-24 15:54:56 +0600

    add Role and Permission models with relationships

---
+++ src/Models/Permission.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Subalroy22\LaravelRolePermissions\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    protected $fillable = ['name', 'guard_name'];
+
+    /**
+     * Users that have this permissions
+     */
+    public function users():BelongsToMany
+    {
+        return $this->belongsToMany(
+            config('auth.providers.users.model') ?? \App\Models\User::class,
+            'permission_user'
+        );
+    }
+
+    /**
+     * Roles that have this permission
+     */
+    public function roles():BelongsToMany
+    {
+        return $this->belongsToMany(
+            Role::class,
+            'role_has_permissions'
+        );
+    }
+
+}
---
+++ src/Models/Role.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Subalroy22\LaravelRolePermissions\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    protected $fillable = ['name', 'guard_name'];
+
+    /**
+     * Users that have this roles
+     */
+    public function users():BelongsToMany
+    {
+        return $this->belongsToMany(
+            config('auth.providers.users.model') ?? \App\Models\User::class,
+            'role_user'
+        );
+    }
+
+    /**
+     * Permissions that associated with this role
+     */
+    public function permissions():BelongsToMany
+    {
+        return $this->belongsToMany(
+            Permission::class,
+            'role_has_permissions'
+        );
+    }
+}
